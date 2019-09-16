3ac261d2526a7a81669ea967cee0e40f6b9ab3d7
Bola Naguib <bolanaguib@gmail.com>
2019-09-16 13:38:43 +0200

    [ initial template ]

---
+++ src/template/footer.php
@@ -0,0 +1,86 @@
+
+ <footer class="section_theme-gradient" style="padding:50px 0px;">
+   <div class="uk-container uk-container-large">
+     <div class="uk-child-width-1-4@m uk-child-width-1-1" uk-grid>
+       <div class="">
+         <h4> Title </h4>
+         <hr class="hr_type-section">
+         <ul class="uk-list">
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Home</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Portofolio</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Pricing</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Request a Quote</a></li>
+         </ul>
+       </div>
+       <div class="">
+         <h4> Title </h4>
+         <hr class="hr_type-section">
+         <ul class="uk-list">
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Home</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Portofolio</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Pricing</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Request a Quote</a></li>
+         </ul>
+       </div>
+       <div class="">
+         <h4> Title </h4>
+         <hr class="hr_type-section">
+         <ul class="uk-list">
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Home</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Portofolio</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Pricing</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Request a Quote</a></li>
+         </ul>
+       </div>
+       <div class="">
+         <h4> Title </h4>
+         <hr class="hr_type-section">
+         <ul class="uk-list">
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Home</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Portofolio</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Pricing</a></li>
+           <li><a class=" uk-button uk-button-text navbar__link" href="#">Request a Quote</a></li>
+         </ul>
+       </div>
+     </div>
+     <hr>
+     <div class="uk-child-width-1-2" uk-grid>
+       <div class="uk-text-left@m uk-text-center">
+         <span> © xvxlabs 2019, Egypt </span> <br>
+
+       </div>
+       <div class="uk-text-right@m uk-text-center">
+         <ul class="uk-navbar-nav uk-flex uk-flex-right">
+           <li><a class="sociallinks" href="http://behance.net/bolanaguib1f24"><i class="fab fa-behance-square"></i>
+
+             </a></li>
+           <li><a class="sociallinks" href="https://www.facebook.com/EgySoldier"><i class="fab fa-facebook-square"></i>
+
+             </a></li>
+           <li>
+             <a class="sociallinks" href="https://www.linkedin.com/in/bola-naguib/"><i class="fab fa-linkedin"></i></a>
+           </li>
+           <li class="uk-padding-right">
+             <a class="sociallinks" href="https://wa.me/201023989330"><i class="fab fa-whatsapp-square"></i></a>
+           </li>
+
+
+
+
+
+         </ul>
+       </div>
+
+     </div>
+
+
+   </div>
+ </footer>
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
