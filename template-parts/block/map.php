ad76988a760dd4809a8973e9cf0f2fe475f31721
Bola <bolanaguib@gmail.com>
2020-06-18 07:13:04 +0200

    [ Removed ] Maps to test speed

--- template-parts/block/map.php
+++ template-parts/block/map.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 // ACF Fields
-$map = get_field( 'map' );
-$section_id = get_field( 'section_id' );
-$full_width = get_field( 'full_width' );
+$map = get_field('map');
+$section_id = get_field('section_id');
+$full_width = get_field('full_width');
 
-?> <!-- START Map --><!-- START section --><section id="<?php echo $section_id;?>" class="iframe <?php if($full_width): ?> full-width <?php else: ?> <?php endif; ?>"> <?php echo $map ;?> </section><!-- END section -->
+?> <!-- START Map --><!-- START section --><!-- <section id="<?php echo $section_id; ?>" class='iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>'> <?php echo $map; ?> </section> --><!-- END section -->
