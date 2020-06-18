e6589048c04277e53268f75e76f06d01a872cb7e
Bola <bolanaguib@gmail.com>
2020-06-18 07:40:23 +0200

    [ added ] image instead of map iframe

--- src/template/template-parts/block/map.php
+++ src/template/template-parts/block/map.php
@@ -3,12 +3,30 @@
 $map = get_field('map');
 $section_id = get_field('section_id');
 $full_width = get_field('full_width');
-
+$map_image = get_field('map_image');
+$map_url = get_field('map_url');
 ?>
 <!-- START Map -->
-<!-- START section -->
-<!-- <section id="<?php echo $section_id; ?>" class='iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>'>
-    <?php echo $map; ?>
+<?php if ($map_image) : ?>
+    <!-- START section -->
+    <section id="<?php echo $section_id; ?>" class='iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>'>
+        <a href="<?php echo $$map_url; ?>">
+            <!-- START mapContainer -->
+            <div class='mapContainer' style="background-image: url('<?php echo $map_image['url'] ?>');     min-height: 450px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;">
 
-</section> -->
-<!-- END section -->
+            </div>
+            <!-- END mapContainer -->
+        </a>
+    </section>
+    <!-- END section -->
+<?php else : ?>
+    <!-- START section -->
+    <section id="<?php echo $section_id; ?>" class='iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>'>
+        <?php echo $map; ?>
+
+    </section>
+    <!-- END section -->
+<?php endif; ?>
--- template-parts/block/map.php
+++ template-parts/block/map.php
@@ -3,5 +3,9 @@
 $map = get_field('map');
 $section_id = get_field('section_id');
 $full_width = get_field('full_width');
-
-?> <!-- START Map --><!-- START section --><!-- <section id="<?php echo $section_id; ?>" class='iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>'> <?php echo $map; ?> </section> --><!-- END section -->
+$map_image = get_field('map_image');
+$map_url = get_field('map_url');
+?> <!-- START Map --> <?php if ($map_image) : ?> <!-- START section --><section id="<?php echo $section_id; ?>" class="iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>"><a href="<?php echo $$map_url; ?>"><!-- START mapContainer --><div class="mapContainer" style="background-image: url('<?php echo $map_image['url'] ?>');     min-height: 450px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;"></div><!-- END mapContainer --></a></section><!-- END section --> <?php else : ?> <!-- START section --><section id="<?php echo $section_id; ?>" class="iframe <?php if ($full_width) : ?> full-width <?php else : ?> <?php endif; ?>"> <?php echo $map; ?> </section><!-- END section --> <?php endif; ?>
